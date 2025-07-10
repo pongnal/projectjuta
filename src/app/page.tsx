69ce6d112f88cd52bd87700054c5d9df2a6ec833
zainal <133510409+pongnal@users.noreply.github.com>
2025-07-10 09:37:56 +0800

    first setup install next js

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <h1 className="text-3xl font-bold">Hello world!</h1>{" "}
+        <h1>hantu mak kau</h1>
+      </div>
+    </div>
+  );
+}
